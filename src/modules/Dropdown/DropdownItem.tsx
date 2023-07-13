ae49a75962c17bfcdd77f41fcac2edeaa736ef4d
Vitalii Popov <symbx.live@gmail.com>
2023-07-14 01:01:34 +0300

    Add `as` prop for dropdown item (router-link)

--- src/modules/Dropdown/DropdownItem.tsx
+++ src/modules/Dropdown/DropdownItem.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { computed, defineComponent, inject, PropType, withModifiers } from "vue";
+import { computed, defineComponent, inject, PropType, withModifiers, resolveComponent } from "vue";
 import { Image } from "../../elements/Image";
 import { Label } from "../../elements/Label";
 import { computeKeyOnly } from "../../utils/classNameHelper";
@@ -16,10 +16,17 @@ export default defineComponent({
     item: [Object, String, Number],
     label: Object,
     text: String,
-    onSelect: Function as PropType<(event: InputEvent) => void>
+    onSelect: Function as PropType<(event: InputEvent) => void>,
+    as: String,
   },
   setup(props, { emit }) {
     const { state, hide } = inject('useDropdown') as any
+
+    let elementType: any = props.as || 'a'
+
+    if (props.as === 'router-link') {
+      elementType = resolveComponent(props.as)
+    }
 
     const computedClass = computed(() => {
       return clsx(
@@ -33,24 +40,18 @@ export default defineComponent({
       emit('select', props.item ? (typeof props.item === 'object' ? props.item.value : props.item) : props.text)
     }
 
-    return {
-      computedClass,
-      onClick
-    }
-  },
-  render() {
-    return (
-      <div
-        class={this.computedClass}
-        onClick={withModifiers(this.onClick, ['stop'])}
+    return () => (
+      <elementType
+        class={computedClass.value}
+        onClick={withModifiers(onClick, ['stop'])}
       >
-        {this.flag && <i class={`${this.flag} flag`}></i>}
-        {this.icon && <i class={`${this.icon} icon`}></i>}
-        {this.image && <Image {...this.image}></Image>}
-        {this.label && <Label {...this.label}></Label>}
-        {this.description && <span class="description">{this.description}</span>}
-        {this.text}
-      </div>
+        {props.flag && <i class={`${props.flag} flag`}></i>}
+        {props.icon && <i class={`${props.icon} icon`}></i>}
+        {props.image && <Image {...props.image}></Image>}
+        {props.label && <Label {...props.label}></Label>}
+        {props.description && <span class="description">{props.description}</span>}
+        {props.text}
+      </elementType>
     )
   }
 })
